5b26071ebb61fa58725d5d4572605bef98808650
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-08-19 20:59:49 +1000

    feat: support empty fallbacks (#146)

---
+++ tests/fixtures/input/fallbackUnpolyfilled.ts
@@ -0,0 +1,4 @@
+// This is not polyfilled: https://github.com/jspm/jspm-core/tree/main/nodelibs/browser
+import * as traceEvents from 'node:trace_events';
+
+console.log(typeof traceEvents);
---
+++ tests/scenarios/fallbackPolyfilled.test.ts
@@ -0,0 +1,28 @@
+import esbuild, { type BuildOptions } from 'esbuild';
+
+import { assertFileContent, buildAbsolutePath, createEsbuildConfig } from '../util';
+
+import type { NodePolyfillsOptions } from '../../dist';
+
+function createConfig(pluginOptions?: NodePolyfillsOptions): BuildOptions {
+	return createEsbuildConfig(
+		{
+			format: 'iife',
+			entryPoints: [buildAbsolutePath('./fixtures/input/fallbackPolyfilled.ts')],
+		},
+		pluginOptions,
+	);
+}
+
+describe('Fallback Test (Polyfilled)', () => {
+	test("GIVEN a file that imports a node builtin that isn't defined in the modules config THEN provide an empty fallback", async () => {
+		const config = createConfig({
+			fallback: 'empty',
+			modules: ['constants'], // This ensures you can still polyfill other modules
+		});
+
+		await esbuild.build(config);
+
+		await assertFileContent('./fixtures/output/fallbackPolyfilled.js');
+	});
+});
