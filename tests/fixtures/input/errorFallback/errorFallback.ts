f6da65b12cf27a8813f4273d69d0b23034032f5a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2023-08-28 20:23:54 +1000

    test: ensure tree-shaken modules avoid fallback (#156)

--- tests/fixtures/input/errorFallback/errorFallback.ts
+++ tests/fixtures/input/errorFallback/errorFallback.ts
@@ -2,6 +2,11 @@ import * as crypto from 'node:crypto';
 import * as path from 'node:path';
 import * as trace_events from 'node:trace_events';
 
+// Ensure unused polyfills don't trigger the fallback
+import { message } from './unusedPolyfill';
+
 console.log(crypto);
 console.log(trace_events);
 console.log(path);
+
+console.log(message);
